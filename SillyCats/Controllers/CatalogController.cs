03f9128424d44d22aee715b62980f845f0fb1ac1
Colcolat <jj.zapatabuenfil@gmail.com>
2026-05-08 00:53:35 -0600

    Feat the user can now upload images of the Items, added new classes in Add.cshtml and Details.cshtml for showcasing images.

--- SillyCats/Controllers/CatalogController.cs
+++ SillyCats/Controllers/CatalogController.cs
@@ -1,10 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting; // Necesario para IWebHostEnvironment
+using Microsoft.AspNetCore.Http;    // Necesario para IFormFile
+using System.IO;                    // Necesario para manejo de archivos y rutas
+using System;                       // Necesario para Guid
+using System.Threading.Tasks;       // Necesario para programación asíncrona (Task)
+using System.Linq;
+using System.Collections.Generic;
 using SillyCats.Models;
 
 namespace SillyCats.Controllers;
 
 public class CatalogController : Controller
 {
+    // 1. Declaramos la variable para acceder al entorno (wwwroot)
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    // 2. Inyectamos la dependencia en el constructor
+    public CatalogController(IWebHostEnvironment webHostEnvironment)
+    {
+        _webHostEnvironment = webHostEnvironment;
+    }
+
     private static List<Item> _items = new()
     {
         new Item
@@ -24,8 +40,7 @@ public class CatalogController : Controller
             Breed = "Domestic Shorthair",
             Gender = "Male",
             Personality = "Stupid & an Idiot",
-            Description =
-                "He gained notoriety for an incident in which his feeder completely flooded with water. During this time, Stupid Idiot was unable to eat from it. A human tried to offer him 10 hotdogs, as well as some cat food, but Stupid Idiot rejected every one.",
+            Description = "He gained notoriety for an incident in which his feeder completely flooded with water. During this time, Stupid Idiot was unable to eat from it. A human tried to offer him 10 hotdogs, as well as some cat food, but Stupid Idiot rejected every one.",
             ImageUrl = "/images/creatures/Idiot.png"
         }
     };
@@ -34,7 +49,7 @@ public class CatalogController : Controller
     {
         var result = string.IsNullOrEmpty(Name)
             ? _items
-            : _items.Where(i => i.Name.ToUpper() == Name.ToUpper()).ToList(); //Para ahorrar complicaciones
+            : _items.Where(i => i.Name.ToUpper() == Name.ToUpper()).ToList(); // Para ahorrar complicaciones
         
         ViewBag.Names = _items.Select(it => it.Name).Distinct().ToList();
         ViewBag.CurrentName = Name;
@@ -51,13 +66,38 @@ public class CatalogController : Controller
     {
         return View();
     }
-
+    
     [HttpPost]
-    public IActionResult Add(Item item)
+    public async Task<IActionResult> Add(Item item, IFormFile imageFile)
     {
-        item.Id = _items.Count + 1;
+        if (imageFile != null && imageFile.Length > 0)
+        {
+            string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images", "creatures");
+            
+            if (!Directory.Exists(uploadsFolder))
+            {
+                Directory.CreateDirectory(uploadsFolder);
+            }
+            
+            string uniqueFileName = Guid.NewGuid().ToString() + "_" + imageFile.FileName;
+            string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+            
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(fileStream);
+            }
+            
+            item.ImageUrl = "/images/creatures/" + uniqueFileName;
+        }
+        else
+        {
+            item.ImageUrl = "/images/creatures/default.png";
+        }
+        
+        item.Id = _items.Count > 0 ? _items.Max(i => i.Id) + 1 : 1; 
+        
         _items.Add(item);
+        
         return RedirectToAction("Index");
     }
-
 }
